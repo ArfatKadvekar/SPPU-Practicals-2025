43384096ffc38287e5fc941952e52fa65b8fb1fe
ArfatKadvekar <arfatkadvekar1305@gmail.com>
2025-03-27 12:56:19 +0530

    ass7

--- MPL/practical03.asm
+++ MPL/practical03.asm
@@ -1,13 +1,13 @@
 %macro rw 3
     mov rax, %1        
     mov rdi, 1          
-    mov rsi, %2         
-    mov rdx, %3         
+    mov rsi, %2        
+    mov rdx, %3        
     syscall
 %endmacro
 
 %macro exit 1
-    mov rax, %1         
+    mov rax, %1        
     mov rdi, 0          
     syscall
 %endmacro
@@ -15,33 +15,57 @@
 section .data
 counter db 05h
 arr dq 1234567891234567h, 0c325487652154687h, 0a789654574521354h, 0912357895314455h, 3545645445666556h
+msg db "Largest Element: ", 10
+msglen equ $-msg
+
+count db 10h
+
 
 section .bss
 
 result resb 02
+answer resb 16h
 
 section .text
     global _start
 
 _start:      
-	mov rdi, arr
-	mov al, 00h    
-iter: 
-    cmp al, [rdi]
-    jae x
-    
-    mov al, [rdi]
-    
- x: 
-    inc rdi
+mov rdi, arr
+mov rax, 00h    
+iter:
+    cmp rax, [rdi]
+    jae y
+   
+x: mov rax, [rdi]
+   
+y:
+    add rdi, 8
     dec byte[counter]
     jnz iter
-       
-y: 
-exit 60                 
 
+    mov [answer], rax
+    call h2a
+    jmp ex
 
-;TODO
-;need to improve this code especially that hex to ascii part
-
-
+h2a:
+    mov byte[count], 10h
+    mov rbx, answer
+iter1:
+    rol rax, 04h
+    mov dl, al
+    and dl, 0Fh
+    cmp dl, 09h
+    jle xi
+    add dl, 07h  
+   
+ xi:
+    add dl, 30h
+    mov [rbx], dl
+    inc rbx
+    dec byte[count]
+    jnz iter1
+    rw 01, msg, msglen
+    rw 01, answer, 16
+    ret
+ex:
+exit 60
---
+++ MPL/practical07.asm
@@ -0,0 +1,145 @@
+%macro rw 3
+    mov rax, %1        
+    mov rdi, 1          
+    mov rsi, %2         
+    mov rdx, %3         
+    syscall
+%endmacro
+
+%macro exit 1
+    mov rax, %1         
+    mov rdi, 0          
+    syscall
+%endmacro
+
+section .data
+count db 10h
+count2 db 20h
+endl db "h", 10
+endlen equ $-endl
+
+pmsg db "The processor is in protected mode",10
+pmsglen equ $-pmsg
+
+tr db "TR: " 
+trlen equ $-tr 
+
+ldt db "LDT: " 
+ldtlen equ $-ldt
+
+msw db "MSW: " 
+mswlen equ $-msw
+
+idtr db "IDTR: " 
+idtrlen equ $-idtr
+
+gdtr db "GDTR: " 
+gdtrlen equ $-gdtr
+
+
+section .bss
+    result resb 20
+    val resb 20
+    trv resb 4
+    ldtv resb 4
+    gdtv resb 6
+    idtv resb 6
+    mswv resb 4
+    v1 resb 4
+    v2 resb 2
+
+section .text
+    global _start
+
+_start:   
+
+smsw rax 
+bt rax, 0
+jc pm
+
+pm:
+   rw 01, pmsg, pmsglen
+   rw 01, tr, trlen
+   str [trv]
+   mov rax, [trv]
+   call htoa16
+   
+   rw 01, ldt, ldtlen
+   sldt [ldtv]
+   mov rax, [ldtv]
+   call htoa16
+   
+   rw 01, msw, mswlen
+   smsw [mswv]
+   mov rax, [mswv]
+   call htoa16
+   
+   rw 01, gdtr, gdtrlen
+   sgdt [gdtv]
+   mov eax, dword[gdtv]
+   mov dword[v1], eax
+   call htoa32
+   
+   mov ax, word[gdtv+4]
+   mov word[v2], ax
+   call htoa16
+   
+   rw 01, idtr, idtrlen
+   sidt [idtv]
+   mov eax, dword[idtv]
+   mov dword[v1], eax
+   call htoa32
+   
+   mov ax, word[idtv+4]
+   mov word[v2], ax
+   call htoa16
+   jmp ex
+   
+debug1:
+ 
+htoa16: 
+    mov rbx, result
+    mov byte[count], 10h
+iterh: 
+    rol rax, 04h
+    mov dl, al
+    and dl, 0Fh
+    cmp dl, 09h
+    jle xh
+    add dl, 07h  
+    
+ xh: 
+    add dl, 30h
+    mov [rbx], dl
+    inc rbx
+    dec byte[count]
+    jnz iterh
+ret
+
+htoa32: 
+    mov rbx, result
+    mov byte[count2], 20h
+iterh2: 
+    rol rax, 04h
+    mov dl, al
+    and dl, 0Fh
+    cmp dl, 09h
+    jle xh2
+    add dl, 07h  
+    
+ xh2: 
+    add dl, 30h
+    mov [rbx], dl
+    inc rbx
+    dec byte[count]
+    jnz iterh2
+    rw 01, result, 32
+    rw 01, endl, endlen
+ret
+
+ex: exit 60                 
+
+
+
+
+
