69930239f6f6effa9f530e297ff34b1bea7a246b
Arfat <arfatkadvekar1305@gmail.com>
2025-04-25 10:05:52 +0530

    final

--- MPL/practical06.asm
+++ MPL/practical06.asm
@@ -1,46 +1,168 @@
 %macro rw 3
-    mov rax, %1        
-    mov rdi, 1          
-    mov rsi, %2         
-    mov rdx, %3         
+    mov rax, %1
+    mov rdi, 1
+    mov rsi, %2
+    mov rdx, %3
     syscall
 %endmacro
 
-%macro exit 1
-    mov rax, %1         
-    mov rdi, 0          
+%macro exit 0
+    mov rax, 60
+    mov rdi, 0
     syscall
 %endmacro
 
 section .data
+    opm db "1. HEX to BCD",
+    db 10, 13, "2. BCD to HEX",
+    db 10, 13, "3. EXIT", 10, 13,
+    db   "Enter Choice: "
+    opmlen equ $-opm
+   
+    msg1: db "Enter 5-digit BCD number: "
+    msg1len equ $-msg1
 
-menu db "========== Menu ==========",10,13, "1. Addition", 10, 13,"2. Substraction", 10, 13,"3. Multiplication", 10, 13,"4. Unsigned Multiplication", 10, 13,"5. Division", 10, 13,"6. Unsigned Division", 10, 13,"7. Exit", 10, 13,"Enter Choice: "
-menulen equ $-menu
+    msg2: db "Hex equivalent result is: "
+    msg2len equ $-msg2
 
-choice db 01h
+    msg3: db "Enter 4-digit HEX number: "
+    msg3len equ $-msg3
 
-sn1 dq 23456789abcdef01h
-sn2 dq 10h
+    msg4: db "BCD equivalent result is: "
+    msg4len equ $-msg4
+   
+    nl db 10
+
+
+section .bss
+    op resb 1
+bcdinp resb 6
+    temp resb 1
+    count resb 1
+    ahexinp resb 5
+    hexinp resb 4
+
+
 
 section .text
-    global _start
+global _start
+_start:
 
-_start:      
-	rw 01, menu, menulen
-	rw 00, choice, 01h
-	cmp byte[choice], 31h
-	jne next1
-	jmp debug
-next1: 
-	mov al, byte[sn1]
-	add al, byte[sn2]
-debug:
+    back:
+
+    rw 1, opm, opmlen
+    rw 0, op, 2
+
+    cmp byte[op],  '1'
+    je case1
+   
+    cmp byte[op], '2'
+    je case2
+
+    exit
+
+    b1:
+        rw 1, nl, 1
+        rw 1, nl, 1
+        jmp back
+
+    case1:
+        rw 1, msg3, msg3len
+        rw 0, ahexinp, 5
+        call A2H
+       
+        push rax
+        rw 1, msg4, msg4len
+        pop rax
+        call H2B
+        jmp b1
+   
+    case2:
+        rw 1, msg1, msg1len
+        rw 0, bcdinp, 6
+        call B2H
+
+        rw 1, msg2, msg2len
+        call H2A
+        jmp b1
+
+    exit
 
 
+H2B:
+    mov byte[count], 0
 
-exit 60                 
+    mov bx, 10
 
+    up2:
+        xor rdx, rdx
+        div rbx
+        push rdx
+        inc byte[count]
+        cmp ax, 0
+        jne up2
 
+    up3:
+        pop rdx
+        add dl, 30H
+        mov [temp], dl
+        rw 1, temp, 1
+        dec byte[count]
+        jnz up3
+    ret
 
+B2H:
+    mov rsi, bcdinp
+    mov rcx, 05H
 
+    xor rax, rax
+    mov ebx, 0AH
+    u1:
+        xor rdx, rdx
+        mul ebx
+        mov dl, [rsi]
+        sub dl, 30H
+        add rax, rdx
+        inc rsi
+        loop u1
+    mov rbx, rax
+ret
 
+H2A:
+    mov byte[count], 8
+    loop1:
+        rol ebx, 4
+        mov al, bl
+        and al, 0FH
+        cmp al, 09H
+        jg a1
+        add al, 30H
+        jmp a2
+   
+        a1: add al, 37H
+   
+        a2: mov [temp], al
+            rw 1, temp, 1
+            dec byte[count]
+            jnz loop1
+    ret
+
+A2H:
+    mov rsi, ahexinp
+    mov rcx, 04H
+
+    xor rax, rax
+   
+    loop2:
+        shl rax, 4
+        mov bl, [rsi]
+        inc rsi
+
+        cmp bl, 39h                        
+        jbe down
+        sub bl, 7h                        
+        down:
+        sub bl, 30h  
+        or al, bl
+        loop loop2
+    ret
--- MPL/practical10.asm
+++ MPL/practical10.asm
@@ -1,50 +1,129 @@
-%macro rw 3
-    mov rax, %1        
-    mov rdi, 1          
-    mov rsi, %2         
-    mov rdx, %3         
-    syscall
-%endmacro
-
-%macro exit 1
-    mov rax, %1         
-    mov rdi, 0          
-    syscall
-%endmacro
-
 section .data
-	fname db "demo.txt", 0
-	
+    msg1 db "Your Number is: "
+    msg1len equ $-msg1
+
+    msg2 db "Factorial is: "
+    msg2len equ $-msg2
+
+msg3 db "Factorial is 01"
+msg3len equ $-msg3
+nl db 10
+
+count db 0
+       
 section .bss
-  global buffer 
-  buffer resb 100
-  fd resb 64
-  
+  num resb 1
+temp resb 1
+res resb 8
+
+
+%macro rw 3
+    mov rax, %1
+    mov rdi, %1
+    mov rsi, %2
+    mov rdx, %3
+    syscall
+%endmacro
+
+
 section .text
-    global _start
+global _start
+_start:
+    pop rbx  ; Discrading argument count
+    pop rbx  ; Discarding program name
+    pop rbx  ; Storing number in rbx
 
-_start: 
+; storing in num variable
+    mov al, byte[rbx]
+    mov [num], al
 
-open: 
-	mov rax, 02
-	mov rdi, fname
-	mov rsi, 02h
-	mov rdx, 0777h
-	syscall
-	
-	mov fd, rax
-	
-read:
-	mov rax, 00
-	mov rdi, [fd]
-	mov rsi, buffer
-	mov rdx, 100
-	syscall
+; Printing number
+    rw 1, msg1, msg1len
+rw 1, num, 1
 
-close:
-	mov rax, 03
-	mov rdi, [fd]
-	syscall
+; Handling 0 and 1
+cmp byte[num], 31h
+je one
+cmp byte[num], 30h
+je one
 
-	rw 01, buffer 
-ex: exit 60
+
+; Converting to hex
+mov bl, byte[num]
+call A2H
+
+xor rax, rax
+xor rdx, rdx
+
+rw 1, nl, 1
+
+mov al, byte[num]
+
+; Preparing stack.
+iter:
+push rax
+inc byte[count]
+dec rax
+cmp rax, 1
+jne iter
+
+push rax
+xor rax, rax
+
+pop rax
+
+ft:
+pop rdx
+mul rdx
+dec byte[count]
+jnz ft
+
+mov qword[res], rax
+
+rw 1, msg2, msg2len
+call H2A
+
+jmp exit
+
+one:
+rw 1, nl, 1
+rw 1, msg3, msg3len
+jmp exit
+
+; Procs
+
+A2H:                              
+cmp bl, 39h                        
+jbe down
+sub bl, 7h                        
+down:
+sub bl, 30h                        
+mov [num], bl                      
+ret
+
+H2A:
+mov rbx, qword[res]
+mov byte[count], 8
+loop1:
+rol ebx, 4
+mov al, bl
+and al, 0FH
+cmp al, 09H
+jg a1
+add al, 30H
+jmp a2
+
+a1: add al, 37H
+
+a2: mov [temp], al
+rw 1, temp, 1
+dec byte[count]
+jnz loop1
+
+ret
+
+exit:
+mov rax, 60
+mov rdi, 0
+syscall
+
