8846b0ff23ddc6d58afb1964bf908f316e512893
Arfat <arfatkadvekar1305@gmail.com>
2025-04-28 22:40:42 +0530

    final

--- MPL/practical01.asm
+++ MPL/practical01.asm
@@ -15,14 +15,14 @@
 section .data
 count db 05h
 
-msg db "Enter the Numbers:",10,13
+msg db "Enter the Numbers:",10
 msglen equ $-msg
 
-msg1 db "The Numbers are:",10,13
+msg1 db "The Numbers are:",10
 msglen1 equ $-msg1
 
 section .bss
-    num resb 25   
+    num resb 85   
            
 section .text
     global _start
@@ -31,19 +31,21 @@ _start:
     mov rbx, num
     rw 01, msg, msglen        
 
-x_loop:
-    rw 00, rbx, 5     
-    add rbx, 5     
+input_loop:
+    rw 00, rbx, 17     
+    add rbx, 17     
     dec byte[count]        
-    jnz x_loop
+    jnz input_loop
      
+    mov byte[count], 5 ; reset counter
     rw 01, msg1, msglen1  
-    mov rbx, num         
-y_loop:
-    rw 01, rbx, 5   
-    add rbx, 5      
-    dec byte[count]        
-    jnz y_loop       
+    mov rbx, num 
+
+output_loop:
+    rw 01, rbx, 17   
+    add rbx, 17      
+    dec byte[count] 
+    jnz output_loop       
 
 exit 60                 
 
